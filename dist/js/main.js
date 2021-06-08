047ecc691f8577a3134a5677098031a3f136e280
RicardoEsparza <riky.paramore@gmail.com>
2021-06-07 20:36:17 -0500

    Finalización del sidebar
    Se completo el diseño del sidebar responsive y del navbar y el content main

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,41 @@
-const showSidebar = (toogleClass, sidebar, navbar) =>{
+const showSidebar = (toogleClass, sidebar, btnMenu) =>{
     const toggle = document.getElementById(toogleClass),
     sideMenu = document.getElementById(sidebar),
-    menu = document.getElementById(navbar)
+    menu = document.getElementById(btnMenu)
 
     if(toggle && sideMenu && menu){
         toggle.addEventListener('click',()=>{
             sideMenu.classList.toggle('active')
+            menu.classList.toggle('bx-x')
+
             menu.classList.toggle('navbarActive')
         })
     }
 }
 
-showSidebar('btnCollapse','sidebar', 'menunav')
+const showNotificacion = () =>{
+    const toggle = document.getElementById('dropbtn'),
+    content2 = document.getElementById('items-perfil'),
+    content = document.getElementById('dropdown-content')
+    if(toggle){
+        toggle.addEventListener('click',()=>{
+            content.classList.toggle('activarMenuNoti')
+            content2.classList.remove('showPerfil')
+        })
+    }
+}
+const showPerfil = () =>{
+    const toggle = document.getElementById('imgBtn'),
+    content = document.getElementById('items-perfil'),
+    content2 = document.getElementById('dropdown-content')
+    if(toggle){
+        toggle.addEventListener('click',()=>{
+            content.classList.toggle('showPerfil')
+            content2.classList.remove('activarMenuNoti')
+        })
+    }
+}
+
+showSidebar('btnCollapse','sidebar', 'btnCollapse')
+showNotificacion()
+showPerfil()
